2d3e8ffd5bc24c1ea25ab33566edf524790f7b1f
Szász Szilárd <szasz.szilard7@gmail.com>
2019-03-25 19:52:02 +0200

    some surpluss is removed now...

--- public/js/app.js
+++ public/js/app.js
@@ -36684,8 +36684,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! d:\Munka\xampp\htdocs\lynx\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! d:\Munka\xampp\htdocs\lynx\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! d:\Munka\xampp\htdocs\Laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! d:\Munka\xampp\htdocs\Laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
